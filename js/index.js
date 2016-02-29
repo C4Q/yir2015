e6c7c2b49c8076c7248f800fb5039d0b879d573d
Dan Vingo <danvingo@gmail.com>
2016-02-29 07:30:49 -0500

    Added simple coreography

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 import s from '../styles/main.scss'
 const qs = document.querySelector.bind(document)
 const appContainerSelector = '.App__container'
+const thingsToMoveLeft = [
+  '.SectionHome__text',
+  '.SectionHome__img',
+  '.SectionHome__container',
+  '.SectionOverview__container',
+  '.SectionStats__container',
+  '.SectionApps__container',
+  '.SectionPartners__container'
+]
 
 qs('#moveLeftButton').addEventListener('click', moveLeft)
 qs('#moveRightButton').addEventListener('click', moveRight)
 
 function moveLeft(e) {
   e.preventDefault()
-  const appContainer = qs(appContainerSelector)
-  appContainer.classList.add('left')
+  thingsToMoveLeft.map(i => qs(i)).forEach(i => {
+    i.classList.add('left')
+  })
 }
 
 function moveRight(e) {
   e.preventDefault()
-  const appContainer = qs(appContainerSelector)
-  appContainer.classList.remove('left')
+  thingsToMoveLeft.map(i => qs(i)).forEach(i => {
+    i.classList.remove('left')
+  })
 }
